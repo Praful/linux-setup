dd24152b822ae20c55c80dba483532e1b47fff9b
Praful Kapadia <git@kaptech.co.uk>
2023-01-23 18:33:35 +0000

    add vim install lib script

--- scripts/install-linux-utils.sh
+++ scripts/install-linux-utils.sh
@@ -1,6 +1,6 @@
 sudo apt install nodejs
-sudo apt install vim
-sudo apt install gvim
+# sudo apt install vim
+# sudo apt install gvim
 # sudo apt install smbclient
 # sudo apt install samba
 sudo apt install dos2unix
@@ -8,9 +8,12 @@ sudo apt install git
 sudo apt install exuberant-ctags
 sudo apt install npm
 sudo apt install fzf
+sudo apt install ripgrep
 sudo apt install fd-find
+sudo apt install ruby
+sudo apt install perl
+sudo apt install lua5.4
 
-
-npm install -g eslint
-npm install -g nodemon
-npm install -g concurrently
+# npm install -g eslint
+# npm install -g nodemon
+# npm install -g concurrently
---
+++ scripts/install-vim-compile-libs.sh
@@ -0,0 +1,17 @@
+sudo apt install git
+sudo apt install make
+sudo apt install clang
+sudo apt install libtool-bin
+sudo apt install libxt-dev
+sudo apt install libgtk-3-dev
+
+sudo apt install libpython3-dev
+sudo apt install lua5.4-dev
+sudo apt install libperl-dev
+sudo apt install ruby-dev
+
+
+# git clone https://github.com/vim/vim.git
+# cd vim/src
+make reconfig
+sudo make install
