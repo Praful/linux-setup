f1523a79978deae955deffe7677f98418d8bc888
Praful Kapadia <git@kaptech.co.uk>
2023-12-18 10:38:18 +0000

    vim compile now resets git and uses copy of makefile

--- scripts/update-vim.sh
+++ scripts/update-vim.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 #
-cd /home/praful/data/dev/open-source/vim
+pushd $DATA/dev/open-source/vim
+
+# discard local changes since we have only one, which is backed up
+git reset --hard
 
 # stash changes, usually just makefile
-git stash
+# git stash
 
 # get updates
 git pull
 
-#restore changes
-git stash pop
-
 cd src
+
+# restore backed up file
+cp $DATA/dev/projects/vim-config/Makefile.pk ./Makefile
+
+#restore changes
+# git stash pop
+
 
 # clean previous build
 sudo make distclean
 sudo make
 sudo make install
+
+popd
